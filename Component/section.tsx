fa43e15ed8b91e4b0da4108ad671258281f40e98
PG <90617690+Patricigithinji@users.noreply.github.com>
2025-12-25 18:53:51 +0300

    Add Section component with title and children props

--- Component/section.tsx
+++ Component/section.tsx
@@ -1,1 +1,17 @@
+type Props = {
+  title: string
+  children: React.ReactNode
+}
 
+export default function Section({ title, children }: Props) {
+  return (
+    <section className="mb-14">
+      <h2 className="mb-4 text-lg font-semibold tracking-tight">
+        {title}
+      </h2>
+      <div className="text-sm text-neutral-400">
+        {children}
+      </div>
+    </section>
+  )
+}
